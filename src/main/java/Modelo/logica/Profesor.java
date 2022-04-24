68bab5aef40b55a639c11cf38894d32ffd8485f6
Jorsua-Gs <73625908+Jorsua-Gs@users.noreply.github.com>
2022-04-23 20:01:20 -0600

    Delete persona
    ayuda

--- src/main/java/Modelo/logica/Profesor.java
+++ src/main/java/Modelo/logica/Profesor.java
@@ -4,13 +4,63 @@ package modelo.logica;
  *
  * @author Vic
  */
-public class Profesor extends Persona {
+public class Profesor {
 
-    public Profesor() {
+    public String getId() {
+        return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Usuario getUsu() {
+        return usu;
+    }
+
+    public void setUsu(Usuario usu) {
+        this.usu = usu;
+    }
+
+    private String id;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private Usuario usu;
+
     public Profesor(String id, String nombre, String telefono, String email, Usuario usu) {
-        super(id, nombre, telefono, email, usu);
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.usu = usu;
+    }
+
+    public Profesor() {
     }
 
 }
